dfb5407e035a2bdfc4e8f16900b2ba0075f7cb96
ishige <hiroyukiishige@hotmail.com>
2023-06-20 07:00:45 +0700

    update

--- main.py
+++ main.py
@@ -69,3 +69,6 @@ def sampleform():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+#TODO file upload
--- reference.py
+++ reference.py
@@ -5,4 +5,8 @@ https://qiita.com/Bashi50/items/9bb5ca4a519c17caf475
 How to use CSS and Bootstrap:
 https://engineer-lifestyle-blog.com/code/python/flask-bootstrap-css-implementation/
 
+File upload:
+https://blog.miguelgrinberg.com/post/handling-file-uploads-with-flask
+
+
 """
